4fc454246a8d4588dadef2f850cc38b51ee08d8b
Kyoz004 <k104.umarudabezt@gmail.com>
2026-04-26 22:06:47 +0700

    Revert "feat: Restructure home page and fix image paths"
    This reverts commit 464652a311174ac85fddba2bb365da4833a1e7eb.

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,20 +1,11 @@
-// src/app/page.tsx
+import { ExperienceTimeline } from "./containers/experience-timeline";
 
-import { MyUniverse } from "@/containers/my-universe";
-import { ProjectCardsCarousel } from "@/app/projects/project-list";
-
-export default function Home() {
+const ExperiencePage = () => {
   return (
-    <main className="container mx-auto px-4 py-8 md:py-16">
-      {/* Hero section "Hello, I'm Kiet Do" */}
-      <section className="mb-16 md:mb-24">
-        <MyUniverse />
-      </section>
-
-      {/* Project Carousel section */}
-      <section>
-        <ProjectCardsCarousel />
-      </section>
-    </main>
+    <div>
+      <ExperienceTimeline />
+    </div>
   );
-}
+};
+
+export default ExperiencePage;
